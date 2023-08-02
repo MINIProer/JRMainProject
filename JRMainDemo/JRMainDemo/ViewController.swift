9eb1a89aeaa629f5691de29284af7f8c7d974c2f
jiangshulun <jiangshulun@joyme.sg>
2023-08-02 11:46:35 +0800

    更新配置

--- JRMainDemo/JRMainDemo/ViewController.swift
+++ JRMainDemo/JRMainDemo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import JRSubDemo
 
 class ViewController: UIViewController {
 
---
+++ JRMainDemo/Pods/Target Support Files/JRSubDemo/JRSubDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JRSubDemo : NSObject
+@end
+@implementation PodsDummy_JRSubDemo
+@end

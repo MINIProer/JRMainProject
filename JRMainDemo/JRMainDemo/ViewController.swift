1c0436819c8107bef43dc2f6d575f785a880291f
jiangshulun <jiangshulun@joyme.sg>
2023-08-02 12:26:11 +0800

    更新代码

--- JRMainDemo/JRMainDemo/ViewController.swift
+++ JRMainDemo/JRMainDemo/ViewController.swift
@@ -14,9 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let f = JRFuckTool()
+        let fuck = JRFuckTool()
     }
-
 
 }
 
